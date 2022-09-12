8423ca84231e913a43b88e73bfa14882eb2d306c
Sanghyun Lee <hammerha@gmail.com>
2022-09-12 21:40:33 +0200

    Finish Part 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,19 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
+
+    # This representation is used throughout Django's admin.
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -11,3 +21,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
