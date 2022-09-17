a6a7c3615c34b67527dd0fab0233c733bd0754c6
Sanghyun Lee <hammerha@gmail.com>
2022-09-17 18:33:17 +0200

    Add ModelAdmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+
+# Changing an order in the form
+# class QuestionAdmin(admin.ModelAdmin):
+# fields = ['pub_date', 'question_text']
+
+
+# Add fieldsets
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
