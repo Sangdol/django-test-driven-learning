9468a951c6daff147168a616bf18e08ac7490bf0
Sanghyun Lee <hammerha@gmail.com>
2022-09-17 18:49:44 +0200

    Add sorting for was_published_recently()

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
 import datetime
 
+# contrib contains the common functionality for web dev
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -12,6 +14,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # This is needed since sorting by a function is not supported.
+    @admin.display(boolean=True, ordering='pub_date', description='Published recently?')
     def was_published_recently(self):
         now = timezone.now()
         return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= now
