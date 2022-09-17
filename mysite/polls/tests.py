a58ec03f8be7d2fb5737fa0c3cc3c507a9a8d40c
Sanghyun Lee <hammerha@gmail.com>
2022-09-17 18:19:48 +0200

    Add QuestionIndexViewTests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
 
@@ -7,7 +8,6 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date is the future.
@@ -16,3 +16,32 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
 
         self.assertIs(future_question.was_published_recently(), False)
+
+
+# Continue writing tests from here...
+# https://docs.djangoproject.com/en/4.1/intro/tutorial05/#testing-our-new-view
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        # Why does this break?
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_future_quesiton(self):
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
