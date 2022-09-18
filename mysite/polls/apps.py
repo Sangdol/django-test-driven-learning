6ee2640ff11ece6c819088ca114a1b83010af3b3
Sanghyun Lee <hammerha@gmail.com>
2022-09-18 19:34:45 +0200

    Add doc link comment

--- mysite/polls/apps.py
+++ mysite/polls/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# https://docs.djangoproject.com/en/4.1/ref/applications/#django.apps.AppConfig.label
 class PollsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "polls"
